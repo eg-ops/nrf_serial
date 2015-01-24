4bcfef6e25a190855e117ebf6e9d59682794fef0
g-gabber <i7gabber@gmail.com>
2015-01-24 23:54:26 +0100

    first nrf test

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "stm8s.h"
 #include "stm8s_gpio.h"
 #include "stm8s_spi.h"
+#include "nrf24l01.h"
 
 
 #define CE GPIO_PIN_3 // Port C
@@ -10,7 +11,6 @@
 #define MISO GPIO_PIN_7 // Port C
 
 #define IRQ GPIO_PIN_2 // Port D
-
 
 
 
@@ -30,14 +30,7 @@ static void delay(uint32_t t)
 
 
 
-uint8_t nrf24l01p_spi_rw(uint8_t value) {
-	while (SPI_GetFlagStatus(SPI_FLAG_TXE) == RESET) {
-	}
-	SPI_SendData(value);
-	while (SPI_GetFlagStatus(SPI_FLAG_RXNE) == RESET) {
-	}
-	return SPI_ReceiveData();
-}
+
 
 int main( void )
 {
@@ -54,12 +47,20 @@ int main( void )
   GPIO_Init(GPIOC, SCK, GPIO_MODE_OUT_PP_LOW_FAST);
   
   SPI_DeInit();
-  SPI_Init(SPI_FIRSTBIT_MSB, SPI_BAUDRATEPRESCALER_32, SPI_MODE_MASTER, SPI_CLOCKPOLARITY_LOW, SPI_CLOCKPHASE_1EDGE, SPI_DATADIRECTION_2LINES_FULLDUPLEX, SPI_NSS_SOFT, 0);
+  SPI_Init(SPI_FIRSTBIT_MSB, SPI_BAUDRATEPRESCALER_32, SPI_MODE_MASTER, SPI_CLOCKPOLARITY_LOW, SPI_CLOCKPHASE_1EDGE, SPI_DATADIRECTION_2LINES_FULLDUPLEX, SPI_NSS_SOFT, 7);
   SPI_Cmd(ENABLE);
   
-  uint8_t val = nrf24l01p_spi_rw(0);
+  {
   
-  val++;
+     uint8_t addr[5] = {0};
+     uint8_t status = 0;
+    uint8_t ch = get_channel();
+     nrf24l01p_read(TX_ADDR, addr, sizeof(addr));
+     nrf24l01p_read_reg(STATUS, &status, 1);
+     ch++;
+     status++;
+    
+  }
   
   while(1){
     delay(20000);
@@ -72,3 +73,29 @@ int main( void )
   
   return 0;
 }
+
+
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval : None
+  */
+
+u8* file_;
+void assert_failed(u8* file, u32 line)
+{ 
+  file_ = file;
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
