02dbdd2e22abe6548694417f32b4e80e48b22458
g-gabber <i7gabber@gmail.com>
2015-01-25 13:04:00 +0100

    interrupts testing

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "stm8s.h"
+#include "stm8s_it.h"
 #include "stm8s_gpio.h"
 #include "stm8s_spi.h"
+#include "stm8s_exti.h"
 #include "nrf24l01.h"
 
 
@@ -30,6 +32,33 @@ static void delay(uint32_t t)
 
 
 
+uint8_t nrf24l01p_spi_rw(uint8_t value) {
+	while (SPI_GetFlagStatus(SPI_FLAG_TXE) == RESET) {
+	}
+	SPI_SendData(value);
+	while (SPI_GetFlagStatus(SPI_FLAG_RXNE) == RESET) {
+	}
+	return SPI_ReceiveData();
+}
+
+void nrf24l01p_ce_high(){ GPIO_WriteHigh(GPIOC, CE); }
+void nrf24l01p_ce_low(){ GPIO_WriteLow(GPIOC, CE); }
+
+void nrf24l01p_csn_high(){ GPIO_WriteHigh(GPIOC, CSN); }
+
+void nrf24l01p_csn_low(){ GPIO_WriteLow(GPIOC, CSN); }
+void nrf24l01p_irq_enable(){ enableInterrupts(); }
+void nrf24l01p_irq_disable(){ disableInterrupts(); }
+
+
+
+
+INTERRUPT_HANDLER(EXTI_PORTD_IRQHandler, 6)
+{
+    GPIO_WriteReverse(GPIOD, (GPIO_PIN_4 ) );
+    
+    
+}
 
 
 int main( void )
@@ -46,10 +75,27 @@ int main( void )
   GPIO_Init(GPIOC, CE, GPIO_MODE_OUT_PP_LOW_FAST);
   GPIO_Init(GPIOC, SCK, GPIO_MODE_OUT_PP_LOW_FAST);
   
+  GPIO_Init(GPIOD, IRQ, GPIO_MODE_IN_PU_IT);
+  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOD, EXTI_SENSITIVITY_FALL_ONLY);
+  
   SPI_DeInit();
   SPI_Init(SPI_FIRSTBIT_MSB, SPI_BAUDRATEPRESCALER_32, SPI_MODE_MASTER, SPI_CLOCKPOLARITY_LOW, SPI_CLOCKPHASE_1EDGE, SPI_DATADIRECTION_2LINES_FULLDUPLEX, SPI_NSS_SOFT, 7);
   SPI_Cmd(ENABLE);
   
+  
+  set_pipe_size(0,4);
+  pipe_enable(0);
+  set_channel(125);
+  enable_irqs();
+  set_rx_mode();
+  //set_tx_mode();
+  
+  nrf24l01p_ce_high();
+  power_up();
+  
+  
+  
+  enableInterrupts();
   {
   
      uint8_t addr[5] = {0};
@@ -62,16 +108,24 @@ int main( void )
     
   }
   
+  uint8_t data[] = { 0x31, 0x32, 0x33, 0x34};
+  
   while(1){
     delay(20000);
-    GPIO_WriteReverse(GPIOD, (GPIO_PIN_3 | GPIO_PIN_4) & rand());
+    GPIO_WriteReverse(GPIOD, (GPIO_PIN_3 ) & rand());
    
+     //power_up();
+  
+  
+
+  //nrf24l01p_write(FLUSH_TX, 0, 0);
+  //nrf24l01p_write(W_TX_PAYLOAD, (uint8_t*)&data, sizeof(data));
     
     
   }
   
   
-  return 0;
+  return 0; 
 }
 
 
